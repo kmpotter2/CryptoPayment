7274e4c05e635e577c51207047d22cc7357a17b0
kmpotter2 <97268901+kmpotter2@users.noreply.github.com>
2022-10-27 21:46:47 -0700

    Update fintech_finder.py
    import crypto_wallet as cw

--- fintech_finder.py
+++ fintech_finder.py
@@ -30,6 +30,7 @@ from dataclasses import dataclass
 from typing import Any, List
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+import crypto_wallet as cw
 ################################################################################
 # Step 1:
 # Import Ethereum Transaction Functions into the Fintech Finder Application
