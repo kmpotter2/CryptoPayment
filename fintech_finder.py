181b21ad448046b69533d95eea24ccf6fe48a6a5
kmpotter2 <97268901+kmpotter2@users.noreply.github.com>
2022-10-27 21:47:37 -0700

    Update fintech_finder.py
    Called the `generate_account` function and save it as the variable `account`

--- fintech_finder.py
+++ fintech_finder.py
@@ -131,7 +131,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = cw.generate_account()
 
 ##########################################
 
